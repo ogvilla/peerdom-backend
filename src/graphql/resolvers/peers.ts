dff5ebef9e45eb4204fedddc57ccc3ccc11cd03f
David Sandoz <david@peerdom.org>
2018-10-18 18:35:19 +0200

    refactor(core): load mission relation when retrieving peers
    Signed-off-by: David Sandoz <david@peerdom.org>

--- src/graphql/resolvers/peers.ts
+++ src/graphql/resolvers/peers.ts
@@ -4,6 +4,6 @@ import {Peer} from '../../entity/peer';
 export const peersResolver = {
   async peers() {
     const repository = getRepository(Peer);
-    return await repository.find();
+    return await repository.find({relations: ['mission']});
   }
 };
