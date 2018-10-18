d3d820f1017923fd13c5d8a18a85f7bb337f13fe
David Sandoz <david@peerdom.org>
2018-10-18 15:09:04 +0200

    refactor(core): add __resolveType for Node interface
    Signed-off-by: David Sandoz <david@peerdom.org>

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -2,6 +2,19 @@ import {helloResolver} from './resolvers/hello';
 import {messageResolver} from './resolvers/message';
 
 export const resolvers = {
+  Node: {
+    __resolveType(obj, context, info) {
+      if (obj.holders) {
+        return 'Role';
+      }
+
+      if (obj.children) {
+        return 'Circle';
+      }
+
+      return null;
+    }
+  },
   Query: {
     ...helloResolver,
     ...messageResolver
