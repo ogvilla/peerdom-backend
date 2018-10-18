bfbbceffb2d11045bab5077daae47b24e91b65b0
Tobias Leugger <toby@peerdom.org>
2018-10-18 13:46:01 +0200

    feat(database): add Postgres connection
    Code mostly generated by `typeorm init --database postgres`,
    part of that also a dummy-entity User
    
    Signed-off-by: Tobias Leugger <toby@peerdom.org>

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # peerdom-backend
 ecosystem of digital tools that helps organisations evolve as a decentralized, values-based collective of peers (backend)
+
+# Setup
+
+## Install deps
+- NodeJS (see .nvmrc for which version) & run npm install
+- PostgreSQL (10.x)
+
+## Setup PostgreSQL user and Table
+
+    sudo su - postgres
+    createuser --pwprompt peerdom
+    createdb -O peerdom peerdom-test
+
+## Config
+Edit ormconfig.json and add the DB settings (user & password)
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
-import {ApolloServer, gql} from 'apollo-server-express';
+import 'reflect-metadata';
+import {ApolloServer} from 'apollo-server-express';
+import {createConnection} from 'typeorm';
 
 import {resolvers} from 'graphql/resolvers';
 import {Mutation} from 'graphql/types/mutation';
@@ -15,4 +17,23 @@ server.applyMiddleware({app});
 
 const port = 3000;
 
-app.listen({port}, () => console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`));
+createConnection()
+  .then(async connection => {
+    // import {User} from './entity/User';
+    // console.log('Inserting a new user into the database...');
+    // const user = new User();
+    // user.firstName = 'Timber';
+    // user.lastName = 'Saw';
+    // user.age = 25;
+    // await connection.manager.save(user);
+    // console.log('Saved a new user with id: ' + user.id);
+    //
+    // console.log('Loading users from the database...');
+    // const users = await connection.manager.find(User);
+    // console.log('Loaded users: ', users);
+
+    app.listen({port}, () => {
+      console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`);
+    });
+  })
+  .catch(error => console.log(error));
