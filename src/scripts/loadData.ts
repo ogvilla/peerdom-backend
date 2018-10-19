109daac80f45bcd0f1220b75976c0bf395456db9
Joanna Salathé <joanna@peerdom.org>
2018-10-19 09:38:55 +0200

    refactor(fixtures): drop db in loadData script before inserting data
    Signed-off-by: Joanna Salathé <joanna@peerdom.org>

--- src/scripts/loadData.ts
+++ src/scripts/loadData.ts
@@ -1,22 +1,27 @@
 import {createConnection} from 'typeorm';
 import {Tenant} from '../entity/tenant';
 
+// get connection
 createConnection()
   .then(async connection => {
     const tenant = new Tenant();
     tenant.id = '00000000-0000-0000-0000-000000000000';
     tenant.tenantName = 'Test';
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tenant)
-      .values(tenant)
-      .onConflict(`("id") DO NOTHING`)
-      .execute();
+    // drop db
+    connection.synchronize(true).then(async res => {
+      // insert data
+      await connection
+        .createQueryBuilder()
+        .insert()
+        .into(Tenant)
+        .values(tenant)
+        .onConflict(`("id") DO NOTHING`)
+        .execute();
 
-    console.log('Loading tenants from the database...');
-    const tenants = await connection.manager.find(Tenant);
-    console.log('Loaded tenants: ', tenants);
-    await connection.close();
+      console.log('Loading tenants from the database...');
+      const tenants = await connection.manager.find(Tenant);
+      console.log('Loaded tenants: ', tenants);
+      await connection.close();
+    });
   })
   .catch(error => console.log(error));
