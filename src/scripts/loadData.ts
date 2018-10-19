c79e8093e9e6511e4c34e65dbd0949f801a48558
Joanna Salathé <joanna@peerdom.org>
2018-10-19 14:39:39 +0200

    feat(fixtures): add core roles fixtures
    Signed-off-by: Joanna Salathé <joanna@peerdom.org>

--- src/scripts/loadData.ts
+++ src/scripts/loadData.ts
@@ -1,38 +1,36 @@
 import {createConnection} from 'typeorm';
 import * as fs from 'fs';
 
-const entitiesNameToLoad = ['Tenant', 'Peer', 'Mission'];
+const entitiesToLoad = [
+  {name: 'Tenant', jsonFileNames: ['tenants']},
+  {name: 'Peer', jsonFileNames: ['peers']},
+  {name: 'Mission', jsonFileNames: ['missions']},
+  {name: 'Node', jsonFileNames: ['coreRoles']}
+];
 
 /*
  * This script is inspired by https://github.com/jgordor
  * https://github.com/nestjs/nest/issues/409#issuecomment-364639051
 */
-async function getEntities(connection) {
-  console.log('===== GET ENTITIES =====');
-  const entities = [];
-  (await connection.entityMetadatas).forEach(entity => {
-    if (!entitiesNameToLoad.includes(entity.name)) {
-      console.log('Entity ' + entity.name + ' has no fixtures');
-    }
-  });
-  return entities;
-}
 
-async function loadAll(connection, entities) {
+async function loadAll(connection) {
   console.log('===== LOAD DATA =====');
   try {
-    for (const entityName of entitiesNameToLoad) {
-      const repository = await connection.getRepository(entityName);
-      const fixtureFile = `src/fixtures/${entityName}.json`;
-      if (fs.existsSync(fixtureFile)) {
-        const items = JSON.parse(fs.readFileSync(fixtureFile, 'utf8'));
-        await repository
-          .createQueryBuilder(entityName)
-          .insert()
-          .values(items)
-          .execute();
-        console.log('Entity ' + entityName + ' loaded');
+    for (const entityToLoad of entitiesToLoad) {
+      const repository = await connection.getRepository(entityToLoad.name);
+      for (const jsonFileName of entityToLoad.jsonFileNames) {
+        const fixtureFile = `src/fixtures/${jsonFileName}.json`;
+        if (fs.existsSync(fixtureFile)) {
+          console.log(`Uploading content of ${jsonFileName}.json`);
+          const items = JSON.parse(fs.readFileSync(fixtureFile, 'utf8'));
+          await repository
+            .createQueryBuilder(entityToLoad.name)
+            .insert()
+            .values(items)
+            .execute();
+        }
       }
+      console.log(`Entity ${entityToLoad.name} loaded`);
     }
   } catch (error) {
     throw new Error(`ERROR Loading fixtures on test db: ${error}`);
@@ -47,9 +45,8 @@ async function cleanAll(connection) {
   try {
     const connection = await createConnection();
     try {
-      const entities = await getEntities(connection);
       await cleanAll(connection);
-      await loadAll(connection, entities);
+      await loadAll(connection);
       await connection.close();
     } catch (error) {
       await connection.close();
