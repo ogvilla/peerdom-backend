f2078157cf6cf66b6386bff4f902fa0e6f44228c
Joanna Salathé <joanna@peerdom.org>
2018-10-19 12:01:33 +0200

    feat(fixtures): read tenant fixtures from json files
    Signed-off-by: Joanna Salathé <joanna@peerdom.org>

--- src/scripts/loadData.ts
+++ src/scripts/loadData.ts
@@ -1,27 +1,53 @@
 import {createConnection} from 'typeorm';
-import {Tenant} from '../entity/tenant';
+import * as fs from 'fs';
 
-// get connection
-createConnection()
-  .then(async connection => {
-    const tenant = new Tenant();
-    tenant.id = '00000000-0000-0000-0000-000000000000';
-    tenant.tenantName = 'Test';
-    // drop db
+/*
+ * This script is inspired by https://github.com/jgordor
+ * https://github.com/nestjs/nest/issues/409#issuecomment-364639051
+*/
+async function getEntities(connection) {
+  console.log('===== GET ENTITIES =====');
+  const entities = [];
+  (await connection.entityMetadatas).forEach(entity => {
+    console.log('Found entity ' + entity.name);
+    entities.push({name: entity.name, tableName: entity.tableName});
+  });
+  return entities;
+}
+
+async function loadAll(connection, entities) {
+  try {
+    for (const entity of entities) {
+      const repository = await connection.getRepository(entity.name);
+      const fixtureFile = `src/fixtures/${entity.name}.json`;
+      if (fs.existsSync(fixtureFile)) {
+        const items = JSON.parse(fs.readFileSync(fixtureFile, 'utf8'));
+        if (!(Object.keys(items).length === 0 && items.constructor === Object)) {
+          console.log('Entity ' + entity.name + ' loading ' + JSON.stringify(items));
+          await repository
+            .createQueryBuilder(entity.name)
+            .insert()
+            .values(items)
+            .execute();
+        }
+      }
+    }
+  } catch (error) {
+    throw new Error(`ERROR Loading fixtures on test db: ${error}`);
+  }
+}
+
+(async () => {
+  console.log('===== LOAD DATA =====');
+  try {
+    const connection = await createConnection();
+
     connection.synchronize(true).then(async res => {
-      // insert data
-      await connection
-        .createQueryBuilder()
-        .insert()
-        .into(Tenant)
-        .values(tenant)
-        .onConflict(`("id") DO NOTHING`)
-        .execute();
-
-      console.log('Loading tenants from the database...');
-      const tenants = await connection.manager.find(Tenant);
-      console.log('Loaded tenants: ', tenants);
+      const entities = await getEntities(connection);
+      await loadAll(connection, entities);
       await connection.close();
     });
-  })
-  .catch(error => console.log(error));
+  } catch (error) {
+    console.log(error);
+  }
+})();
